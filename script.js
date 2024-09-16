dc70406f629c82946c8e2b8ac322cc8a21803f70
Shristi Choudhary <148475072+ShristiC7@users.noreply.github.com>
2024-09-17 02:12:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+
+$(".skills").click(function(){
+
+    $(".skills").html("<li>HTML</li><li>CSS</li><li>JAVASCRIPT</li><li>BOOTSTRAP</li><li>BLOCKCHAIN</li>").css("color", "rgb(194, 148, 237)").css("font-size", "35px");
+
+  });
+
+  $(".qualification").click(function(){
+
+    $(".qualification").html("<li>GRADUATION -B.TECH CSE - GGITS</li><li> 12 <sup>th</sup> - 89.4%</li><li>10 <sup>th</sup>- 93.5%</li>").css("color", "rgb(194, 148, 237)").css("font-size", "30px");
+
+  });
+  $(".hobby").click(function(){
+
+    $(".hobby").html("<li>Photography</li><li>Travel</li><li>Bike Riding</li><li>Horror Movie</li>").css("color", "rgb(194, 148, 237)").css("font-size", "30px");
+
+  });
